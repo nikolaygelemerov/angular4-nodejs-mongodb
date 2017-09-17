94cebc5e1887aa4ee799ac6ac22ddb5f16a1e6ec
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2017-09-18 00:55:34 +0300

    Create db models with mongoose, save and fetch from db

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    User.findOne({}, function(error, doc) {
+        if (error) {
+            return res.send('Error');
+        }
+
+        res.render('node', {email: doc});
+    });
+});
+
+router.post('/', function(req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'Nikolay',
+        lastName: 'Gelemerov',
+        password: 'secret',
+        email: email
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;

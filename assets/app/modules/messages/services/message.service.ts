890dea0d25e9be59b89f8fd79ab6c6542f686b77
Nikolay Gelemerov <nikolay.gelemerov@imperiaonline.org>
2017-11-01 11:36:23 +0200

    fix frontend user data fetch

--- assets/app/modules/messages/services/message.service.ts
+++ assets/app/modules/messages/services/message.service.ts
@@ -23,7 +23,7 @@ export class MessageService {
         return this.httpService.post(this.messageUrl, message)
             .map((response: Response) => {
                 const result = response.json();
-                const message = new Message(result.obj.content, result.obj.firstName, result.obj._id, result.obj.userId);
+                const message = new Message(result.obj.content, result.obj.user.firstName, result.obj._id, result.obj.user._id);
                 this.messages.push(message);
 
                 return message;
